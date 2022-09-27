a663eb71ecc4644c5d0d6c480faeb5d923800f18
William Davie <101512473+DrDavie1@users.noreply.github.com>
2022-09-27 12:11:54 +0100

    Delete Webscraping NFT transaction data directory

--- Webscraping NFT transaction data/run_to_excel.py
+++
@@ -1,6 +0,0 @@
-from Functions.data_visulization import *
-from set_variables import *
-
-dataframe = txn_database(Contract,Page_factor)
-
-write_excel("IVY BOYS",dataframe)
--- Webscraping NFT transaction data/set_variables.py
+++
@@ -1,5 +0,0 @@
-
-ChromeDriver_Location = "/Users/drdavie/Desktop/Python/Driver/chromedriver"
-
-Contract = "0x809d8f2b12454fc07408d2479cf6dc701ecd5a9f" #string
-Page_factor = 0.01 #float, total pages * page factor = number of pages of transactions scraped.
